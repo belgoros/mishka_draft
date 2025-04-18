561149f7d2e605c156231bff980f8d97b2524db8
belgoros <s.cambour@gmail.com>
2025-04-18 13:05:27 +0200

    Try to add the navbar (WIP)

---
+++ lib/mishka_draft_web/live/home_live.ex
@@ -0,0 +1,15 @@
+defmodule MishkaDraftWeb.HomeLive do
+  use MishkaDraftWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1><Welcome to Home Live!/h1>
+
+    """
+  end
+end
